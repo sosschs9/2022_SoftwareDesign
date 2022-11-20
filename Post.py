164f81c23a2fb1d0f33af297c664ea164f7c1cac
DongYoon Kim <dong2857210@gmail.com>
2022-11-20 18:07:06 +0900

    test

---
+++ Post.py
@@ -0,0 +1,22 @@
+class Post:
+    def __init__(self, postIdx, title, location, detailAddress, pay, commentCnt):
+        self.postIdx = postIdx
+        self.title = title
+        self.location = location
+        self.detailAddress = detailAddress
+        self.pay = pay
+        self.commentCnt = commentCnt
+    
+    def Modify(title, content, location, pay):
+        
+
+    def Delete():
+
+    def getLocation():
+
+    def getPay():
+
+    def getTitle():
+
+    def getCommentCnt():
+
