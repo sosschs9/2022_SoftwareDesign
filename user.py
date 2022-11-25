428f16030b2f23e33612799c6a1fe1ad3d811a11
teakju <wngud012012@gmail.com>
2022-11-26 01:34:54 +0900

    private

--- user.py
+++ user.py
@@ -2,54 +2,97 @@
 
 class User:
   def __init__(self, inputid, inputpw, inputname, inputphonenumber, inputgender):
-    self.id = inputid
-    self.password = inputpw
-    self.name = inputname
-    self.phoneNumber = inputphonenumber
-    self.gender = inputgender
-    self.myPost = []
-    self.myRequestHistory = []
+    self.__id = inputid
+    self.__password = inputpw
+    self.__name = inputname
+    self.__phoneNumber = inputphonenumber
+    self.__gender = inputgender
+    self.__myPost = []
+    self.__myRequestHistory = []
   
   def getUserInfo(self):
-    return (self.id, self.name, self.phoneNumber, self.gender)
-  def getName(self):
-    return self.name
-  def getPhoneNum(self):
-    return self.phoneNumber
-  def getGender(self):
-    return self.gender
+    return (self.__id, self.__name, self.__phoneNumber, self.__gender)
+  def __getName(self):
+    return self.__name
+  def __getPhoneNum(self):
+    return self.__phoneNumber
+  def __getGender(self):
+    return self.__gender
   def getID(self):
-    return self.id
-  def getPassword(self):
-    return self.password
+    return self.__id
+  def __getPassword(self):
+    return self.__password
   
   def update(self, pw, pn):
-    self.password = pw
-    self.phoneNumber = pn
+    self.__password = pw
+    self.__phoneNumber = pn
   def login(self, id, pw):
     pass
   def addMyPost(self, idx):
-    self.myPost.append(idx)
+    self.__myPost.append(idx)
   def addMyComment(self, idx):
     pass  
   def addMyRequest(self, idx):
-    self.myRequestHistory.append(idx)
+    self.__myRequestHistory.append(idx)
   
   
 class Helper(User):
   def __init__(self, inputid, inputpw, inputname, inputphonenumber, inputgender, htype):
-    self.id = inputid
-    self.password = inputpw
-    self.name = inputname
-    self.phoneNumber = inputphonenumber
-    self.gender = inputgender
-    self.myPost = []
-    self.myRequestHistory = []
-    self.myHelpHistory = []
-    self.helptype = htype
+    self.__id = inputid
+    self.__password = inputpw
+    self.__name = inputname
+    self.__phoneNumber = inputphonenumber
+    self.__gender = inputgender
+    self.__myPost = []
+    self.__myRequestHistory = []
+    self.__myHelpHistory = []
+    self.__helpType = htype
+    
+  def getUserInfo(self):
+    return (self.__id, self.__name, self.__phoneNumber, self.__gender, self.__helpType)
+  def __getName(self):
+    return self.__name
+  def __getPhoneNum(self):
+    return self.__phoneNumber
+  def __getGender(self):
+    return self.__gender
+  def getID(self):
+    return self.__id
+  def __getPassword(self):
+    return self.__password
+  
+  def update(self, pw, pn):
+    self.__password = pw
+    self.__phoneNumber = pn
+  def login(self, id, pw):
+    pass
+  def addMyPost(self, idx):
+    self.__myPost.append(idx)
+  def addMyComment(self, idx):
+    pass  
+  def addMyRequest(self, idx):
+    self.__myRequestHistory.append(idx)
     
   def getHelperType(self):
-    return self.helperType
+    return self.__helpType
   
   def addmyHelp(self, idx):
-    self.myHelpHistory.append(idx)
+    self.__myHelpHistory.append(idx)
+  
+'''
+if __name__ == '__main__':
+  u1 = Helper("abd","1234","kim","01092190120",0,1)
+  u2 = User("abc","1234","lee","01012 345678",1)
+  print(u1.getID(), u1.getUserInfo(), u1.getHelperType())
+  print(u2.getID(), u2.getUserInfo())
+  
+  u1.addmyHelp(1)
+  u1.addMyPost(2)
+  u1.addMyRequest(3)
+  
+  u2.addMyPost(4)
+  u2.addMyRequest(5)
+  
+  print(u1._Helper__myHelpHistory, u1._Helper__myPost, u1._Helper__myRequestHistory)
+  print(u2._User__myPost, u2._User__myRequestHistory)
+'''
