2e245c50fe467206439c75e4c5b58d2bc4334af3
akamono312 <jwseok312@gmail.com>
2022-11-26 20:03:45 +0000

    Add message sender

--- messagesender.py
+++ messagesender.py
@@ -1,4 +1,23 @@
+import config
+import matching
+from twilio.rest import Client
+
 class MessageSender:
-    def sendMatching(Matching m):
+    
+    def __init__(self, phoneNumber):
+        self.__phoneNumber = phoneNumber
+
+
+    def sendMatching(matching):
+        account_sid = config.twilio_account_sid
+        auth_token = config.twilio_auth_token
+        client = Client(account_sid, auth_token)
+        _message = type(matching).__name__ + ' Accepted'
+        message = client.messages.create(
+            to = '+82' + PHONE_NUMBER,
+            from_ = config.twilio_from_number,
+            body = _message
+        )
 
     def sendAppointment():
+        pass
