1ba1f515270602419aad6c6d0b067f6dd560374d
송혜경 <sosschs9@naver.com>
2022-12-02 05:40:47 +0900

    hye_gooong commit

--- app.py
+++ app.py
@@ -3,9 +3,83 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# default user 불러오기
+
+# 메인페이지
 @app.route('/')
 def home():
-    return
+    return render_template("main.html")
+
+# 의뢰 게시판 보기
+@app.route('/post_board', methods=['GET'])
+def post_board():
+    # 게시판 리스트 불러오기
+    return render_template('Post/postBoard.html')
+
+# 의뢰 게시글 작성하기
+@app.route('/write_post', methods=['GET', 'POST'])
+def writing_post():
+    if request.method == 'POST':
+
+        return render_template('Post/postBoard.html')
+    else:
+        return render_template('Post/newPost.html')
+
+# 의뢰 게시글 보기
+@app.route('/post/{id}', methods=['GET', 'POST'])
+def view_post():
+    # 게시글 불러오기
+
+    if request.method == 'POST':
+        # 댓글 작성
+        return render_template('Post/readPost.html')
+    else:
+        return render_template('Post/newPost.html')
+
+# 도우미 매칭
+@app.route('/select_helper', methods=['GET'])
+def request_matching():
+    return render_template('Matching/newMatching.html')
+
+@app.route('/select_helper/accompany', methods=['GET', 'POST'])
+def accompany_matching():
+    if request.method == 'POST':
+        # 유저 정보 불러오기
+
+        return render_template('check.html')
+    else:
+        return render_template("Matching/accompanyMatching.html")
+
+@app.route('/select_helper/counsel', methods=['GET', 'POST'])
+def counsel_matching():
+    if request.method == 'POST':
+        # 유저 정보 불러오기
+
+        return render_template('check.html')
+    else:
+        return render_template("Matching/counselMatching.html")
+
+@app.route('/select_helper/safe', methods=['GET', 'POST'])
+def safe_matching():
+    if request.method == 'POST':
+        # 유저 정보 불러오기
+
+        return render_template('check.html')
+    else:
+        return render_template("Matching/safeMatching.html")
+
+# 도우미 신청 목록 보기
+@app.route('/matching_board', methods=['GET'])
+def matching_board():
+    # 목록 리스트 불러오기
+    return render_template('Matching/matchingBoard.html')
+
+# 개별 매칭 보기
+@app.route('/matching/{id}', methods=['GET'])
+def view_matching():
+    # 매칭 내용 불러오기
+    return render_template('Matching/readMatching.html')
+
 
 if __name__ == '__main__':
     app.run()
---
+++ post.py
@@ -0,0 +1,134 @@
+from abc import *
+
+"""abstract class"""
+
+
+class Writing(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self, writerID, date, contents):
+        self.__writerID = writerID
+        self.__date = date
+        self.__contents = contents
+
+    #    @abstractmethod
+    #    def delete(self):
+    #        pass
+
+    @abstractmethod
+    def modify(self):
+        pass
+
+    @abstractmethod
+    def getDate(self):
+        pass
+
+    @abstractmethod
+    def getWriterID(self):
+        pass
+
+
+class Post(Writing):
+    """ commentIdx : 댓글 번호"""
+    __commentIdx = 0
+    commentList = {}
+
+    def __init__(self, writerID, date, contents, postIdx, title, location, detailAddress, pay):
+        self.__writerID = writerID
+        self.__date = date
+        self.__contents = contents
+        self.__postIdx = postIdx
+        self.__title = title
+        self.__location = location
+        self.__detailAddress = detailAddress
+        self.__pay = pay
+        self.commentList = {}
+        self.__commentIdx = 0
+
+    """ Post안에 Comment가 있으므로 """
+
+    class Comment(Writing):
+        def __init__(self, writerID, date, contents, commentIdx):
+            self.__writerID = writerID
+            self.__date = date
+            self.__contents = contents
+            self.__commentIdx = commentIdx
+
+    """ DM Layer 후에 완성"""
+
+    #    def delete():
+    #        pass
+
+    def modify(self, contents):
+        self.__contents = contents
+
+    def getDate(self):
+        return self.__date
+
+    def getWriterID(self):
+        return self.__writerID
+
+    def getComment(self):
+        return self.__contents
+
+    """
+    def getCommentIdx(self):
+        return self.__commentIdx
+    """
+
+
+def createComment(self, commentWriterID, commentDate, commentContents):
+    self.__commentIdx += 1
+    self.commentList[self.__commentIdx] = Post.Comment(commentWriterID, commentDate, commentContents, self.__commentIdx)
+
+
+def modify(self, title, content, location, pay):
+    self.__title = title
+    self.__content = content
+    self.__location = location
+    self.__pay = pay
+
+
+""" DM Layer 후에 완성"""
+
+
+#    def delete():
+#        pass
+
+def getDate(self):
+    return self.__date
+
+
+def getWriterID(self):
+    return self.__writerID
+
+
+def getLocation(self):
+    return self.__location
+
+
+def getPay(self):
+    return self.__pay
+
+
+def getTitle(self):
+    return self.__title
+
+
+def getCommentCnt(self):
+    return len(self.commentList)
+
+
+"""
+A = Post(1, "20221124", "aaa", 2, "title", "location", "address", 10000)
+A.createComment(10, "20221125", "bbbbb")
+A.createComment(20, "20221126", "ccccc")
+A.createComment(30, "20221127", "ddddd")
+B = Post(2, "20191124", "AAA", 3, "title2", "location2", "address2", 20000)
+B.createComment(100, "20191125", "BBBBB")
+print(A.getCommentCnt())
+print(B.getCommentCnt())
+A.commentList.pop(1)
+print(A.commentList[2].getDate())
+print(A.commentList[3].getDate())
+print(B.commentList[1].getWriterID())
+"""
