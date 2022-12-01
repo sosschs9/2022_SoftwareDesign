79463e6d2b66cd300170ef7ed97ebbf5377e3597
JYN523 <87629686+JYN523@users.noreply.github.com>
2022-12-01 10:43:22 +0900

    add basic(Address API)

---
+++ basic.py
@@ -0,0 +1,17 @@
+import requests
+
+service_key = 'devU01TX0FVVEgyMDIyMTEyNjExNDQxMzExMzI2MTA='
+
+class Address:
+    detailAddress:str     # 도로명주소 전체
+    placeName:str   # 건물명
+    region:str      # 시도명
+
+    def __init__(self, detailAddress:str, placeName:str, region:str):
+        self.detailAddress = detailAddress
+        self.placeName = placeName
+        self.region = region 
+
+# 주소 받아오기 / ret: Address
+def getAddress():
+    pass
