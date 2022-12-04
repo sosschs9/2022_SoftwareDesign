b24d75973b871bdc64a7fbbfde02cf68deb4c8b8
JYN523 <87629686+JYN523@users.noreply.github.com>
2022-12-04 17:05:00 +0900

    add login

--- app.py
+++ app.py
@@ -6,38 +6,58 @@ from matching import *
 
 app = Flask(__name__)
 
-# Test user 불러오기
-# 일반 사용자 1
-# userID = "testUser"
-# user = DB_getUser(userID)
-# 일반 사용자 2
-# userID = "testUser2"
-# user = DB_getUser(userID)
-# 병원동행 도우미
-# userID = "helper1"
-# user = DB_getUser(userID)
-# 심리상담 도우미
-userID = "helper2"
-user = DB_getUser(userID)
-# 안전점검 도우미
-# userID = "helper4"
-# user = DB_getUser(userID)
-
-# checkType을 통해 matchingBoard 접근 권한 확인
-isHelper = isHelper(userID)
-if isHelper == False:
+class curUser:
+    isLogin = False
+    userID = None
+    user = None
+    isHelper = False
     checkType = 0
-else:
-    checkType = user.getHelperType()
 
 # 메인페이지
 @app.route('/')
 def home():
-    return render_template("main.html", checkType=checkType)
+    return render_template("main.html", checkType=curUser.checkType, isLogin=curUser.isLogin)
+
+# 로그인
+@app.route('/login_page', methods=['GET', 'POST'])
+def login_page():
+    if request.method == 'GET':
+        return render_template("login.html")
+
+    if request.method == 'POST':
+        inputID = request.form.get('userID')
+        inputPW = request.form.get('userPW')
+        
+        success, result = login(inputID, inputPW)
+        if success == False:
+            # flash(result)
+            return render_template("login.html", checkType=curUser.checkType, message=result, isLogin=curUser.isLogin)
+        else:
+            curUser.userID = result
+            curUser.user = DB_getUser(curUser.userID)
+            curUser.isLogin = True
+            if isHelper(curUser.userID) == False:
+                curUser.checkType = 0
+            else:
+                curUser.checkType = curUser.user.getHelperType()
+            return redirect('/')
+    return render_template("login.html",  checkType=curUser.checkType, message="", isLogin=curUser.isLogin)
+
+# 로그인
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    curUser.isLogin = False
+    curUser.userID = None
+    curUser.user = None
+    curUser.isHelper = False
+    curUser.checkType = 0
+    return redirect('/')
 
 # 의뢰 게시판 보기
 @app.route('/post_board/<int:page>', methods=['GET'])
 def post_board(page):
+    if curUser.isLogin == False:
+        return redirect('/login_page')
     # 게시판 리스트 불러오기
     page_list = getAllPostList(page)
     max_page = getAllPostPageCount()
@@ -47,14 +67,14 @@ def post_board(page):
     block_start = (block_size * block_num) + 1
     block_end = block_start + (block_size - 1)
 
-    return render_template('Post/postBoard.html', checkType=checkType,
-                           page_list=page_list, max_page=max_page, this_page=page, start=block_start, end=block_end)
+    return render_template('Post/postBoard.html', checkType=curUser.checkType,
+                           page_list=page_list, max_page=max_page, this_page=page, start=block_start, end=block_end, isLogin=curUser.isLogin)
 
 # 의뢰 게시글 작성하기
 @app.route('/write_post', methods=['GET', 'POST'])
 def writing_post():
     if request.method == 'POST':
-        writerID = userID
+        writerID = curUser.userID
         title = request.form['title']
         contents = request.form['contents']
         reqDate = request.form['date']
@@ -68,25 +88,27 @@ def writing_post():
 
         return redirect('/post_board/1')
     else:
-        return render_template('Post/newPost.html', checkType=checkType,)
+        return render_template('Post/newPost.html', checkType=curUser.checkType, isLogin=curUser.isLogin)
 
 # 의뢰 게시글 보기
 @app.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def view_post(post_id):
     # 게시글 불러오기
     if request.method == 'POST':
+        if curUser.isLogin == False:
+            render_template("login.html", checkType=curUser.checkType)
         # 댓글 작성
         comment = request.form['comment']
-        createComment(post_id, userID, comment)
+        createComment(post_id, curUser.userID, comment)
 
     post = getPost(post_id)
-    return render_template('Post/readPost.html', checkType=checkType, post=post, userID=userID)
+    return render_template('Post/readPost.html', checkType=curUser.checkType, post=post, userID=curUser.userID, isLogin=curUser.isLogin)
 
 # 의뢰 게시글 삭제하기
 @app.route('/delete_post/<int:post_id>')
 def delete_post(post_id):
     post = getPost(post_id)
-    if post.getWriterID() != userID:
+    if post.getWriterID() != curUser.userID:
         flash("글 삭제 권한이 없습니다.")
         return redirect('/post_board/1')
     
@@ -97,7 +119,7 @@ def delete_post(post_id):
 @app.route('/modify_post/<int:post_id>', methods=['GET', 'POST'])
 def modify_post(post_id):
     post = getPost(post_id)
-    if post.getWriterID() != userID:
+    if post.getWriterID() != curUser.userID:
         flash("글 수정 권한이 없습니다.")
         return redirect('/post_board/1')
     
@@ -106,7 +128,7 @@ def modify_post(post_id):
             flash("해당 게시물이 존재하지 않습니다.")
             return redirect('/post_board/1')
         else:
-            return render_template("Post/modifyPost.html", checkType=checkType, post=post)
+            return render_template("Post/modifyPost.html", checkType=curUser.checkType, post=post, isLogin=curUser.isLogin)
 
     else:
         title = request.form['title']
@@ -128,16 +150,21 @@ def delete_comment(post_id, comment_id):
 
     deleteComment(post_id, comment_id)
     post = getPost(post_id)
-    return render_template('Post/readPost.html', post=post, userID=userID)
+    return render_template('Post/readPost.html', post=post, userID=curUser.userID, isLogin=curUser.isLogin)
 
 # 도우미 매칭
 @app.route('/select_helper', methods=['GET'])
 def request_matching():
-    return render_template('Matching/newMatching.html', checkType=checkType)
+    if curUser.isLogin == False:
+        return redirect('/login_page')
+    return render_template('Matching/newMatching.html', checkType=curUser.checkType, isLogin=curUser.isLogin)
 
 # 병원동행 도우미 신청
 @app.route('/select_helper/accompany', methods=['GET', 'POST'])
 def accompany_matching():
+    if curUser.isLogin == False:
+        render_template("login.html", checkType=curUser.checkType, isLogin=curUser.isLogin)
+
     if request.method == 'POST':
         # userinfo = user.getUserInfo()
         reqDate = request.form['date'] + " " + request.form['time']
@@ -149,19 +176,22 @@ def accompany_matching():
         hospital_addr = Address(address, "", "")
         reason = request.form['reason']
 
-        createAccompany(userID, reqDate, start_addr, hospital_addr, reason)
-        user = DB_getUser(userID)
+        createAccompany(curUser.userID, reqDate, start_addr, hospital_addr, reason)
+        user = DB_getUser(curUser.userID)
 
         matchingList = user.getMyRequest()
         matchingID = matchingList[-1]
 
         return redirect(url_for('check_request', helpType=HelperType.ACCOMPANY, matchingID=matchingID))
     else:
-        return render_template("Matching/accompanyMatching.html", checkType=checkType)
+        return render_template("Matching/accompanyMatching.html", checkType=curUser.checkType, isLogin=curUser.isLogin)
 
 # 심리상담 도우미 요청
 @app.route('/select_helper/counsel', methods=['GET', 'POST'])
 def counsel_matching():
+    if curUser.isLogin == False:
+        render_template("login.html", checkType=curUser.checkType)
+
     if request.method == 'POST':
         # userinfo = user.getUserInfo()
         reqDate = request.form['date'] + " " + request.form['time']
@@ -169,19 +199,22 @@ def counsel_matching():
         addr = Address(address, "", "")
         category = request.form['category']
 
-        createCounsel(userID, reqDate, addr, category)
-        user = DB_getUser(userID)
+        createCounsel(curUser.userID, reqDate, addr, category)
+        user = DB_getUser(curUser.userID)
 
         matchingList = user.getMyRequest()
         matchingID = matchingList[-1]
 
         return redirect(url_for('check_request', helpType=HelperType.COUNSEL, matchingID=matchingID))
     else:
-        return render_template("Matching/counselMatching.html", checkType=checkType)
+        return render_template("Matching/counselMatching.html", checkType=curUser.checkType, isLogin=curUser.isLogin)
 
 # 안전점검 도우미 요청
 @app.route('/select_helper/safe', methods=['GET', 'POST'])
 def safe_matching():
+    if curUser.isLogin == False:
+        render_template("login.html", checkType=curUser.checkType)
+
     if request.method == 'POST':
         # userinfo = user.getUserInfo()
         reqDate = request.form['date'] + " " + request.form['time']
@@ -189,15 +222,15 @@ def safe_matching():
         addr = Address(address, "", "")
         category = request.form['category']
 
-        createSafetyCheck(userID, reqDate, addr, category)
-        user = DB_getUser(userID)
+        createSafetyCheck(curUser.userID, reqDate, addr, category)
+        user = DB_getUser(curUser.userID)
 
         matchingList = user.getMyRequest()
         matchingID = matchingList[-1]
 
         return redirect(url_for('check_request', helpType=HelperType.SAFETYCHECK, matchingID=matchingID))
     else:
-        return render_template("Matching/safeMatching.html", checkType=checkType)
+        return render_template("Matching/safeMatching.html", checkType=curUser.checkType, isLogin=curUser.isLogin)
 
 @app.route('/check_request/<int:helpType>/<int:matchingID>', methods=['GET', 'POST'])
 def check_request(helpType, matchingID):
@@ -208,7 +241,7 @@ def check_request(helpType, matchingID):
         return redirect(url_for('home'))
     else:
         matching = getMatching(matchingID)
-        check['username'] = user.getUserInfo().NAME
+        check['username'] = curUser.user.getUserInfo().NAME
         # 작성자, 날짜, 장소, 상세 내용
         if type(matching) is Accompany:
             check['helpType'] = "병원동행"
@@ -228,7 +261,7 @@ def check_request(helpType, matchingID):
             check['address'] = matching.getAddress().detailAddress
             check['detail'] = "[점검부분] " + matching.getCheckPart()
 
-        return render_template('check.html', matchingID=matchingID, check=check)
+        return render_template('check.html', matchingID=matchingID, check=check, isLogin=curUser.isLogin)
 
 # 매칭 취소/삭제하기
 @app.route('/delete_matching/<int:matchingID>')
@@ -252,8 +285,8 @@ def matching_board(helperType, page):
     block_start = (block_size * block_num) + 1
     block_end = block_start + (block_size - 1)
 
-    return render_template('Matching/matchingBoard.html', checkType=checkType,
-                           page_list=page_list, max_page=max_page, this_page=page, start=block_start, end=block_end)
+    return render_template('Matching/matchingBoard.html', checkType=curUser.checkType,
+                           page_list=page_list, max_page=max_page, this_page=page, start=block_start, end=block_end, isLogin=curUser.isLogin)
 
 # 개별 매칭 보기
 @app.route('/matching/<int:matching_id>', methods=['GET'])
@@ -268,15 +301,16 @@ def view_matching(matching_id):
     elif type(matching) is SafetyCheck:
         helpType = "안전점검 "
 
-    return render_template('Matching/readMatching.html', checkType=checkType, helpType=helpType, matching=matching)
+    return render_template('Matching/readMatching.html', checkType=curUser.checkType, helpType=helpType, matching=matching, isLogin=curUser.isLogin)
 
 @app.route('/consent_matching/<int:matchingID>')
 def consent_matching(matchingID):
-    requestMatching(matchingID, userID)
+    requestMatching(matchingID, curUser.userID)
     # 메시지 전송 + 메시지 전송 예약
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.secret_key = '1234'
+    app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
+
     app.run()
---
+++ database.py
@@ -0,0 +1,225 @@
+import pymongo
+from user import *
+from matching import *
+from post import *
+from basic import *
+
+# DB 연결
+client = pymongo.MongoClient("mongodb+srv://SD:1234@cluster0.kkapwcd.mongodb.net/?retryWrites=true&w=majority")
+db = client['2022SoftwareDesign']
+col_user = db['User']
+col_post = db['Post']
+col_match = db['Matching']
+
+# USER DB
+# 회원 등록 확인 / ret: boolean
+def isRegistered(userID:str):
+    result = col_user.find_one({'_User__id':userID})
+    if result == None:
+        return False
+    else:
+        return True
+
+# 회원 등록
+def addUser(nUser:User):
+    col_user.insert_one(nUser.__dict__)
+
+# 회원 삭제
+def deleteUser(userID:str):
+    col_user.delete_one({'_User__id':userID})
+
+# 회원 찾기 / ret: dictionary
+def findUser(userID:str):
+    return col_user.find_one({'_User__id':userID})
+
+# 회원 정보 수정 / my~ 추가
+def updateUser(nUser:User):
+    element = findUser(nUser.getID())
+    new_element = nUser.__dict__.copy()
+    new_element['_id'] = element['_id'] # '_id': DB내 식별번호
+    deleteUser(nUser.getID())
+    col_user.insert_one(new_element)
+
+# POST DB
+# 새로운 게시글 번호 / ret: int
+def getPostNumber():
+    result = col_post.find().sort('_Post__postIdx', -1).limit(1)
+    for i in result:
+        return i['_Post__postIdx']+1
+    return 1
+
+# 게시글 추가
+def addPost(nPost:Post):
+    element = nPost.__dict__
+    element['_Post__address'] = nPost.getAddress().__dict__
+
+    commentList = []
+    for i in nPost.commentList:
+        commentList.append(i.__dict__)
+    element['commentList'] = commentList
+
+    col_post.insert_one(element)
+
+# 게시글(+댓글) 불러오기 / ret: Post
+def getPost(postIdx:int):
+    result = col_post.find_one({'_Post__postIdx':postIdx})
+
+    temp = result['_Post__address']
+    addr = Address(temp['detailAddress'], temp['placeName'], temp['region'])
+
+    post =  Post(result['_Post__writerID'], result['_Post__date'], result['_Post__contents'], result['_Post__postIdx'], result['_Post__title'], addr, result['_Post__pay'])
+
+    for i in result['commentList']:
+        post.createComment(i['_Comment__writerID'], i['_Comment__date'], i['_Comment__contents'], i['_Comment__commentIdx'])
+
+    return post
+
+# 게시글 삭제
+def deletePost(postNumber:int):
+    col_post.delete_one({'_Post__postNumber':postNumber})
+
+# 게시글 수정
+def updateMatching(post:Post):
+    element = getPost(post.getPostIdx())
+    
+
+    new_element = post.__dict__.copy()
+    new_element['_id'] = element['_id'] # '_id': DB내 식별번호
+    deletePost(post.getPostIdx())
+    col_post.insert_one(new_element)
+
+# 게시글 목록 불러오기(20개) / ret: list
+def getAllPostList(pageNumber:int):
+    maxIdx = pageNumber * 20
+    result = col_post.find().sort('_Post__postIdx', -1).limit(maxIdx)
+    ret = []
+    cnt = 0
+    for i in result:
+        cnt += 1
+        if cnt <= (pageNumber-1)*20:
+            continue
+        else:
+            ret.append(i)
+    return ret
+
+
+# 해당 유저의 게시글 목록 불러오기 / ret: list
+def getUserPostList(postNumList:list):
+    ret = []
+    for i in postNumList:
+        ret.append(col_post.find_one({'_Post__postIdx':i}))
+    return ret
+
+# Matching
+# 매칭 요청 추가
+def addMatching(nMatching:Matching):
+    col_match.insert_one(nMatching.__dict__)
+
+# 매칭 요청 찾기 / ret: Matching(Accompany/Counsel/SafetyCheck)
+def getMatching(matchingIdx:int):
+    result = col_match.find_one({'_Accompany__matchingIdx':matchingIdx})
+    if result != None:
+        return Accompany(result['_Accompany__date'], result['_Accompany__address'], result['_Accompany__reason'], result['_Accompany__hospitalLocation'])
+    result = col_match.find_one({'_Counsel__matchingIdx':matchingIdx})
+    if result != None:
+        return Counsel(result['_Counsel__date'], result['_Counsel__address'], result['_Counsel__category'])
+    result = col_match.find_one({'_SafetyCheck__matchingIdx':matchingIdx})
+    if result != None:
+        return SafetyCheck(result['_SafetyCheck__date'], result['_SafetyCheck__address'], result['_SafetyCheck__checkpart'])
+    return None
+
+# 매칭 Idx 번호 / ret: int
+def getMatchNumber():
+    result = col_match.find().sort().limit(1)
+    for i in result:
+        return i['Accompany__matchingIdx']+1
+    return 0
+
+# 매칭 요청 목록 불러오기(20개) / ret: list
+def getMatchingList_All(pageNumber:int):
+    maxIdx = pageNumber * 20
+    result = col_match.find().sort('_Matching__matchingIdx', -1).limit(maxIdx)
+    ret = []
+    cnt = 0
+    for i in result:
+        cnt += 1
+        if cnt <= (pageNumber-1)*20:
+            continue
+        else:
+            ret.append(i)
+    return ret
+
+def getMatchingList_Type(pageNumber:int, matchType:int):
+    maxIdx = pageNumber * 20
+    result = col_match.find().sort('_Matching__matchingIdx', -1)
+    ret = []
+    cnt = 0
+    for i in result:
+        
+        cnt += 1
+        if cnt > maxIdx:
+            break
+        if cnt <= (pageNumber-1)*20:
+            continue
+        else:
+            ret.append(i)
+    return ret
+
+# 매칭 내용 업데이트(수정/취소/성공)
+def updateMatching(matching:Matching):
+    element = getMatching(matching.getMatchingIdx())
+    new_element = matching.__dict__.copy()
+    new_element['_id'] = element['_id'] # '_id': DB내 식별번호
+    matchType = ''
+    if element['_Accompany__matchingIdx'] != None:
+        matchType = '_Accompany__matchingIdx'
+    elif element['_Counsel__matchingIdx'] != None:
+        matchType = '_Counsel__matchingIdx'
+    else:
+        matchType='_Counsel__matchingIdx'
+    
+    col_match.delete_one({matchType:element[matchType]})
+    col_post.insert_one(new_element)
+
+# test용 main
+if __name__ == '__main__':
+    # deleteUser('testUser')
+    addr = Address("상세주소", '건물명', '지역')
+    nUser = User('testUser2', '1234', '테스트2', '010-1234-5678', True)
+    help = Helper('testHelper', '5678', '헬퍼', '010-0000-0000', True, 1)
+    nMatch = Accompany('20221126', '주소', '이유', '병원위치')
+    nPost = Post(1, "20221124", "aaa", 2, "title", addr, 10000)
+
+    # col_post.delete_many({})
+    # A = Post(1, "20221124", "aaa", 2, "title", addr, 10000)
+    # A.createComment(10, "20221125", "bbbbb", 1)
+    # A.createComment(20, "20221126", "ccccc", 2)
+    # A.createComment(30, "20221127", "ddddd", 3)
+    # addPost(A)
+
+    # result = getPost(2)
+    
+    # print(getPostNumber())
+    # col_post.delete_many({})
+    # # nMatch.getMatchingIdx()
+    # addMatching(nMatch)
+    # addMatching(nMatch)
+
+    # col_post.delete_many({})
+    # for i in range(1, 51):
+    #     nPost = Post('testUser', '20221126', '내용', i, '제목', '위치', '상세주소', '금액')
+    #     addPost(nPost)
+    # print(getPostNumber())  
+
+    # ret = getAllPostList(4)
+    # for i in ret:
+    #     print(i)
+    
+    # print(len(ret))
+    # addUser(user)
+    # modifyUser(nUser)
+    # addUser(nUser)
+    # print(nUser.getUserInfo())
+    # user = findUser('testUser')
+    # print(user.id)
+    # print(isRegistered("testUser"))
